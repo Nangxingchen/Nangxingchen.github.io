feef5a82518962b7c8a99adf0c3cab24afa4d4a5
nanxingchenuio <19075377433@163.com>
2025-01-18 19:02:44 +0800

    版本: 1.0

---
+++ src/consts.ts
@@ -0,0 +1,112 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Site title and description
+export const SITE_LANG = "en";
+export const SITE_TAB = "Frosti";
+export const SITE_TITLE = "Frosti 🧊";
+export const SITE_DESCRIPTION = "A blog template";
+export const DATE_FORMAT = "ddd MMM DD YYYY";
+
+// User profile information
+export const USER_NAME = "EveSunMaple";
+export const USER_SITE = "https://frosti.saroprock.com"; // At the same time, this is also the site retrieved by the i18n configuration.
+export const USER_AVATAR = "/profile.webp";
+
+// Server and transition settings
+export const SERVER_URL = "https://demo.saroprock.com";
+
+// Theme settings
+export const DAISYUI_THEME = {
+  light: "winter",
+  dark: "dracula",
+};
+export const CODE_THEME = {
+  light: "github-light",
+  dark: "github-dark",
+};
+
+// Menu items for navigation
+export const menuItems = [
+  { id: "home", text: "主页", href: "/", svg: "material-symbols:home-outline-rounded", target: "_self" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "material-symbols:info-outline-rounded", target: "_self" }, // About page
+  {
+    id: "blog",
+    text: "博客",
+    href: "/blog",
+    svg: "material-symbols:book-2-outline-rounded",
+    target: "_self",
+    subItems: [
+      {
+        id: "all",
+        text: "所有博客",
+        href: "/blog",
+        svg: "material-symbols:ink-pen-outline-rounded",
+        target: "_self",
+      }, // 所有博客
+      {
+        id: "tech",
+        text: "技术博客",
+        href: "/blog/categories/tech",
+        svg: "material-symbols:deployed-code-outline",
+        target: "_self",
+      }, // Technology category
+      {
+        id: "life",
+        text: "生活博客",
+        href: "/blog/categories/life",
+        svg: "material-symbols:earthquake-rounded",
+        target: "_self",
+      }, // Life category
+    ],
+  }, // Blog page with sub-items
+  {
+    id: "project",
+    text: "项目",
+    href: "/project",
+    svg: "material-symbols:code-blocks-outline",
+    target: "_self",
+  }, // Projects page
+  {
+    id: "friend",
+    text: "关于我",
+    href: "/friend",
+    svg: "material-symbols:supervisor-account-outline-rounded",
+    target: "_self",
+  }, // Friends page
+  {
+    id: "contact",
+    text: "邮箱",
+    href: "mailto:contact.Nxc19075377433@outlook.com", // Contact email
+    target: "_blank", // Open in a new tab
+    svg: "material-symbols:attach-email-outline-rounded",
+  },
+];
+
+// Social media and contact icons
+export const socialIcons = [
+  {
+    href: "#",
+    ariaLabel: "Support my work",
+    title: "Support my work",
+    svg: "ri:cup-line",
+  },
+  {
+    href: "https://github.com/Nangxingchen",
+    ariaLabel: "Github",
+    title: "Github",
+    svg: "ri:github-line",
+  },
+  {
+    href: "https://space.bilibili.com/3546605890308501?spm_id_from=333.999.0.0",
+    ariaLabel: "BiliBili",
+    title: "BiliBili",
+    svg: "ri:bilibili-line",
+  },
+  {
+    href: "#",
+    ariaLabel: "RSS Feed",
+    title: "RSS Feed",
+    svg: "ri:rss-line",
+  },
+];
